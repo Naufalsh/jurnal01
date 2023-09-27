ab887923e19d62d1587dd62641b600631c78ffa7
Naufalsh <145344178+Naufalsh@users.noreply.github.com>
2023-09-27 14:31:10 +0700

    Create jurnal01.java
    first commit

---
+++ jurnal01.java
@@ -0,0 +1,12 @@
+public class jurnal01 {
+public static void main(String[] args) {
+    String myName = "Naufal Maulana Artocarpus Savero";
+    String NIM = "607062300078";
+    String Kelas = "D3RPLA-47-01";
+
+    System.out.println("Name  : " + myName);
+    System.out.println("NIM   : " + NIM);
+    System.out.println("Class : " + Kelas);
+
+    }
+}
